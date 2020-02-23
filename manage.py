c3dd3cd3dd0df2b1a61f979a087ed992f858928d
JorgeMira <jorge.mira.yague@gmail.com>
2020-02-23 18:01:31 +0100

    Fixed bug when launching prod server

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ from flask_script import Manager
 from calories.deploy import gunicorn
 from calories.main import build_database
 from calories.main import create_app, cfg
-from calories.main.config import ProductionConfig
 
 connex_app = create_app()
 
@@ -18,12 +17,14 @@ manager = Manager(app)
 
 @manager.command
 def build_db():
+    """Build the database"""
     build_database.build_db()
 
 
 @manager.command
 def run():
-    if isinstance(cfg, ProductionConfig):
+    """Run the app"""
+    if cfg.__name__ == 'ProductionConfig':
         gunicorn.run(connex_app)
     else:
         connex_app.run(port=cfg.PORT)
@@ -31,7 +32,7 @@ def run():
 
 @manager.command
 def test():
-    """Runs the unit tests."""
+    """Run the unit tests."""
     tests = unittest.TestLoader().discover('calories/test', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
